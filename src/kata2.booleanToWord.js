e54425176bcdc28fcabf161355b0f6efa2ef2499
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-01 21:22:24 +0100

    Completed kata2/booleanToWord test and function

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,3 +1,12 @@
-const booleanToWord = (boolean) => {};
+const booleanToWord = (boolean) => {
+//    if (boolean === true) {
+//        return 'Yes';
+//    } else {
+//        return 'No';
+//    } 
+return boolean ? 'Yes' : 'No';
+};
+
+// refactored code using ternary operator ?
 
 module.exports = booleanToWord;
