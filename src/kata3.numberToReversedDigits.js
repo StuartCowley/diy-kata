b6d33d239b7065bcdeb8cb61fa4f9821990759b5
Korjubzot <korjubzot@gmail.com>
2023-05-02 11:52:25 +0100

    Generalised numberToReversedDigits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,10 +1,10 @@
 const numberToReversedDigits = (number) => {
-  const num1 = 12345;
+//   const num1 = 12345;
   /* const num2 = 6789;
   const num3 = 2468; */
   /* const num = 12345;
   return Array.from(num, Number); */
-  const numArray = num1.toString().split('').map(Number);
+  const numArray = number.toString().split('').map(Number);
   const reversedArray = numArray.reverse();
   return reversedArray;
 };
