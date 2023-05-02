aaf30561b30479476745cec4650bb9ce4df31edf
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-02 19:56:10 +0100

    Populated kata3 tests

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,13 @@
 const { numberToReversedDigits } = require("../src");
 
-describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+describe('numberToReversedDigits', () => {
+  it('returns a reversed array of the numbers digits', () => {
+    expect(numberToReversedDigits(123)).toStrictEqual([3, 2, 1]);
+  });
+  xit('returns 10-15 in reverse as an array', () => {
+    expect(numberToReversedDigits(101112131415)).toStrictEqual([15, 14, 13, 12, 11, 10]);
+  });
+  xit('returns 20-16 in reverse as an array', () => {
+    expect(numberToReversedDigits(1617181920)).toStrictEqual([20, 19, 18, 17, 16]);
+  });
 });
