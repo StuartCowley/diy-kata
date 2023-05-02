3210b946f44bfc953fc9f0d788b16698317844b3
Korjubzot <korjubzot@gmail.com>
2023-05-02 11:48:20 +0100

    Passed numberToReversedDigits test

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,12 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+  const num1 = 12345;
+  /* const num2 = 6789;
+  const num3 = 2468; */
+  /* const num = 12345;
+  return Array.from(num, Number); */
+  const numArray = num1.toString().split('').map(Number);
+  const reversedArray = numArray.reverse();
+  return reversedArray;
+};
 
 module.exports = numberToReversedDigits;
