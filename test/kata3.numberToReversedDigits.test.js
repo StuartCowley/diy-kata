f3a0f08b0ace2bfac6c039518cc8ff8e6b7652e8
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-02 20:12:34 +0100

    Fixed kata3 tests, it now has 3 functioning tests

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -4,10 +4,10 @@ describe('numberToReversedDigits', () => {
   it('returns a reversed array of the numbers digits', () => {
     expect(numberToReversedDigits(123)).toStrictEqual([3, 2, 1]);
   });
-  xit('returns 10-15 in reverse as an array', () => {
-    expect(numberToReversedDigits(101112131415)).toStrictEqual([15, 14, 13, 12, 11, 10]);
+  it('returns 1-10 in reverse as an array', () => {
+    expect(numberToReversedDigits(123456789)).toStrictEqual([9,8,7,6,5,4,3,2,1]);
   });
-  xit('returns 20-16 in reverse as an array', () => {
-    expect(numberToReversedDigits(1617181920)).toStrictEqual([20, 19, 18, 17, 16]);
+  it('returns odd numbers in reverse as an array', () => {
+    expect(numberToReversedDigits(13579)).toStrictEqual([9,7,5,3,1]);
   });
 });
