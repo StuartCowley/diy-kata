9ca4321aa37714e60c37e57d0b69bb8080254307
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-05 20:51:47 +0100

    Completed src/kata4* and test/kata4*

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,11 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  let catYears = 0;
+  let dogYears = 0;
+  if (number > 1) {
+    catYears = 15 + 9 + (number - 2) * 4;
+    dogYears = 15 + 9 + (number - 2) * 5;
+  }
+  return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
