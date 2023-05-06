ad3967315f6041a86e331f1c62320b03b9dfd6d4
Holly Mathieson <hjmathieson@gmail.com>
2023-05-07 00:57:00 +0100

    finished

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,22 @@
 const humanCatDogYears = (number) => {
-	return [1, 15, 15];
+	const human = number;
+	let cat = 0;
+	let dog = 0;
+	const oldAge = number - 2;
+	if (number === 0) {
+		cat = 0;
+		dog = 0;
+	} else if (number === 1) {
+		cat = 15;
+		dog = 15;
+	} else if (number === 2) {
+		cat = 24;
+		dog = 24;
+	} else if (number > 2) {
+		cat = oldAge * 4;
+		dog = oldAge * 5;
+	}
+	return [human, cat, dog];
 };
 
 module.exports = humanCatDogYears;
