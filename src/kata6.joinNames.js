fc1a281931d69d56653b937200cef5a1826c8b06
Jonnie <jonniet23@hotmail.com>
2023-05-06 15:27:37 +0100

    Kata 5 code and test complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,8 @@
-const joinNames = (namesObj) => {};
-
+const joinNames = (namesObj) => {
+  const justNames = namesObj.map((people) => people.name);
+  const lastName = justNames.splice(-1);
+  const firstHalf = justNames.join(", ");
+  const lastStr = lastName.toString();
+  return `${firstHalf} & ${lastStr}`;
+};
 module.exports = joinNames;
