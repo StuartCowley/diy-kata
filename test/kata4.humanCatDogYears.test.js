4bcd9287d55877d54515cf41f8bb76ef39187f6d
Holly Mathieson <hjmathieson@gmail.com>
2023-05-07 00:11:04 +0100

    test correct

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+	test("returns array of human, cat, dog years", () => {
+		// expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+		// expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+		expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+	});
+});
