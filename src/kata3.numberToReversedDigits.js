64aaab47856fab03327c6a3c5570e16c0b3e8d0a
Holly Mathieson <hjmathieson@gmail.com>
2023-05-07 00:00:07 +0100

    finished

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,6 @@
-const numberToReversedDigits = (number) => {};
-
+const numberToReversedDigits = (number) => {
+	const revNum = number.toString().split("").reverse();
+	const revArray = revNum.map((str) => Number(str));
+	return revArray;
+};
 module.exports = numberToReversedDigits;
