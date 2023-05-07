d5a63f167e0ab7415c1984c08c70bcc6c70400d7
Holly Mathieson <hjmathieson@gmail.com>
2023-05-07 02:04:59 +0100

    cleaning up completed steps

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+	// transfer names into an array
+
+	let nameArray = ["Bart", "Lisa", "Maggie"];
+
+	//good from here down!
+	let lastName = nameArray.pop();
+	let nameString = nameArray.join(", ");
+
+	return `${nameString} & ${lastName}`;
+};
 
 module.exports = joinNames;
