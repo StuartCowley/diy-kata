31db84c054c9820f4dbbff3a8194027d910ff73f
Louise <lkosborne@live.com>
2023-05-08 11:14:41 +0100

    refactoring humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,32 +1,24 @@
 const humanCatDogYears = (number) => {
   const humanYears = number;
   function catYears(humanAge) {
-    let catAge = 0;
     if (humanAge <= 1) {
-      catAge = 15;
-    } else if (humanAge <= 2) {
-      catAge = 15 + 9;
-    } else if (humanAge > 2) {
-      const afterTwo = number - 2;
-      catAge = 24;
-      catAge += afterTwo * 4;
+      return 15;
     }
-    return catAge;
-  }
-  function dogYears(humanAge) {
-    let dogAge = 0;
-    if (humanAge <= 1) {
-      dogAge = 15;
-    } else if (humanAge <= 2) {
-      dogAge = 15 + 9;
-    } else if (humanAge > 2) {
-      const afterTwo = number - 2;
-      dogAge = 24;
-      dogAge += afterTwo * 5;
+    if (humanAge <= 2) {
+      return 24;
     }
-    return dogAge;
+    return 24 + (humanAge - 2) * 4;
   }
 
+  function dogYears(humanAge) {
+    if (humanAge <= 1) {
+      return 15;
+    }
+    if (humanAge <= 2) {
+      return 24;
+    }
+    return 24 + (humanAge - 2) * 5;
+  }
   return [humanYears, catYears(number), dogYears(number)];
 };
 
