8629cc231e20655934a79aaf5e51a3d91495abd6
Korjubzot <korjubzot@gmail.com>
2023-05-08 14:18:56 +0100

    Tidied up code

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,10 +1,5 @@
 // eslint-disable-next-line arrow-body-style
 const getEmployerRole = (employeeName, employees) => {
-  //   const findRole = employees.find((employee) => employee.name === employees);
-  //   if (findRole) {
-  //     return findRole;
-  //   }
-  //   return "Human Recommended Reading Assistant";
   const findRole = employees.find(({ name }) => name === employeeName);
   return findRole.role;
 };
