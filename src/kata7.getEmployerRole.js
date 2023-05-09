afd746e834272707935d08c128b9bb98768dc336
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-05-09 17:16:33 +0100

    Completed src/kata7* and test/kata7* ready for code review

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,4 +1,5 @@
 const getEmployerRole = (employeeName, employees) => {
+  // 
   const employee = employees.find((e) => e.name === employeeName);
   return employee ? employee.role : "Employee Not Found";
 };
