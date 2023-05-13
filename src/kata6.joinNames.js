231bb1e9fb00d6c8037df68f455cad86a2a56243
Louise <lkosborne@live.com>
2023-05-13 14:58:32 +0100

    joinNames passed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  let namesString = "";
+  for (let i = 0; i < namesObj.length; i += 1) {
+    if (i === namesObj.length - 1) {
+      namesString += " & " + namesObj[i].name;
+    } else if (i === namesObj.length - 2) {
+      namesString += namesObj[i].name;
+    } else {
+      namesString += namesObj[i].name + ', ';
+    }
+  }
+  return namesString;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,20 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toBe("Bart, Lisa & Maggie");
+  });
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        { name: "Picard" },
+        { name: "Janeway" },
+        { name: "Riker" },
+        { name: "Kirk" },
+        { name: "Archer" },
+      ])
+    ).toBe("Picard, Janeway, Riker, Kirk & Archer");
+  });
 });
