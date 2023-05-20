e582c2ba00340860a5b40f0ad921e8f2a3a836ef
Lois Adea <loisadea@gmail.com>
2023-05-20 15:15:36 +0100

    Complete DIY Kata

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,16 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+  if (number % 3 != 0 && number % 5 != 0) {
+    return number;
+  }
+
+  let res = "";
+  if (number % 3 === 0) {
+    res += "Fizz";
+  }
+  if (number % 5 === 0) {
+    res += "Buzz";
+  }
+  return res;
+};
 
 module.exports = fizzBuzz;
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,19 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  let catYears = 0;
+  let dogYears = 0;
+  if (number >= 1) {
+    catYears += 15;
+    dogYears += 15;
+  }
+  if (number >= 2) {
+    catYears += 9;
+    dogYears += 9;
+  }
+  if (number >= 3) {
+    catYears += (number - 2) * 4;
+    dogYears += (number - 2) * 5;
+  }
+  return [number, catYears, dogYears];
+};
 
 module.exports = humanCatDogYears;
