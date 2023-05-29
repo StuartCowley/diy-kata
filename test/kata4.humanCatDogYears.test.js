af850668e5b722d257d4780ddd641ffe5185f1de
Armghan <armghanz@gmail.com>
2023-05-29 12:05:23 +0100

    Human Cat Dog Years Test Completed

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("returns array of ages for humans, cats and dogs", () => {
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
+  });
+});
