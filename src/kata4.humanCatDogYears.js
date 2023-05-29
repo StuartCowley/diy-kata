7608efa06941187e498045a88c714b4ff4723b06
Armghan <armghanz@gmail.com>
2023-05-29 12:05:52 +0100

    Human Cat Dog Years Completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,10 @@
-const humanCatDogYears = (number) => {};
-
-module.exports = humanCatDogYears;
+const humanCatDogYears = (humanYears) => {
+    let catYears = 0;
+    let dogYears = 0;
+    if (humanYears > 1) {
+      catYears = 15 + 9 + (humanYears - 2) * 4;
+      dogYears = 15 + 9 + (humanYears - 2) * 5;
+    }
+    return [humanYears, catYears, dogYears];
+  };
+  module.exports = humanCatDogYears;
