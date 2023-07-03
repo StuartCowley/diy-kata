66adef6bcdbf364e572950ca227257d4cc9204b9
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-03 11:56:49 +0100

    kata6.joinNames.js

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,14 @@
-const joinNames = (namesObj) => {};
+const joinNames = (names) => {;
+    if (names.length === 0) {
+      return "";
+    } else if (names.length === 1) {
+      return names[0];
+    } else if (names.length === 2) {
+      return names.join(" & ");
+    } else {
+      const last = names.pop();
+      return names.join(", ") + " & " + last;
+    }
+  };
 
 module.exports = joinNames;
