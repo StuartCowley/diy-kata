fe503994366336701e2eed5605a2139a873401b2
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-03 12:01:28 +0100

    humanCatDogYears.test

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe ('should return an array of three numbers', () => {
+it ('should return an array of three numbers when passed one number', () => { 
+    expect (humanCatDogYears(1)).toBe [15, 24, 28];
+    expect (humanCatDogYears(10)).toBe [10, 56, 64];
+})
+});
