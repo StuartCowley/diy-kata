8630cc8d9faa2066a5ef42c93df88d186d496d7c
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-03 11:54:46 +0100

    kata3.numberToReversedDigits.js

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,11 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    const digits = String(number).split('');
+    const reversedDigits = digits.reverse();
+    return reversedDigits.map(digit => Number(digit));
+    
+  
+  const reversed = numberToReversedDigits(12345);
+  console.log(reversed);
+};
 
 module.exports = numberToReversedDigits;
