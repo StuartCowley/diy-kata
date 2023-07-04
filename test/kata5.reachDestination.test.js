be0e0bc0497cca412528223a44cfd2e4d32f852b
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-04 20:05:29 +0100

    reachDestination test

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,9 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("divides distance by speed and rounds the figure up", () => {
+    expect(reachDestination(100, 50)).toBe("2.0");
+    expect(reachDestination(150, 40)).toBe("4.0");
+    expect(reachDestination(75,60)).toBe("1.5");
+  });
 });
