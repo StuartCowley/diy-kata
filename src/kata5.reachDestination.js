6de112f2f46ab30ba17045ec2303ebca4284df8a
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-04 20:02:08 +0100

    reachDestination.js

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+function reachDestination (distance, speed) {
+        const time = (distance / speed);
+        const roundedTime = Math.ceil(time*2) /2;
+         return roundedTime.toFixed(1);
+        };
 
-module.exports = reachDestination;
+module.exports = reachDestination
