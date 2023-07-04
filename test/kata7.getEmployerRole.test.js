d623fa54bb7e8185e530eb755aa5184727db7ed9
lwyers1@hotmail.co.uk <lwyers1@hotmail.co.uk>
2023-07-04 20:04:32 +0100

    getEmployerRole test

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,16 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the role of an employee after their name has been called", () => {
+
+  const employees = [
+    { name: "Spongebob", role: "Fry-Cook" },
+    { name: "Squidward", role: "Front of House" },
+    { name: "Patrick", role: "Customer" },
+  ];
+
+expect(getEmployerRole("Spongebob", employees)).toBe("Fry-Cook");
+expect(getEmployerRole("Squidward", employees)).toBe("Front of House");
+expect(getEmployerRole("Patrick", employees)).toBe("Customer");
+ });
 });
