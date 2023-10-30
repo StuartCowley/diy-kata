fca775ce1605d0203cff8c715928ae79322f8b4d
Ben-H123 <ben_h321@protonmail.com>
2023-10-30 21:34:41 +0000

    finished up to Boolean to Word

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,22 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+    const isDivisibleBy3 = (number % 3) === 0
+    const isDivisibleBy5 = (number % 5) === 0
+    const isDivisibleBy15 = (number % 15) === 0
+
+if (isDivisibleBy15) {
+    return 'FizzBuzz'
+}
+
+if (isDivisibleBy5) {
+    return 'Buzz'
+}
+
+if (isDivisibleBy3) {
+    return 'Fizz'
+}
+
+return number
+
+};
 
 module.exports = fizzBuzz;
