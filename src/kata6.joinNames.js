68ec36346831576e2ba2a7ced8972fbb6cf11340
Abi Jama <abi_jama88@hotmail.com>
2023-11-06 21:12:19 +0000

    fixing the repo

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,11 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+const nameArray = namesObj.map((item) => item.name);
+   if (nameArray.length <= 1) {
+    return nameArray.join("");
+  } else {
+    const lastName = nameArray.pop();
+    return nameArray.join(", ") + " & " + lastName;
+  }
+};
 
 module.exports = joinNames;
