caaed6071b8285363ae676cd2a4bbf51abaaf32e
Abi Jama <abi_jama88@hotmail.com>
2023-11-06 21:19:26 +0000

    joinNames finished

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,5 @@
 const { joinNames } = require("../src");
-
+const inputNames = [{name: "Bart"}, {name: "Lisa"}, {name: "Maggie"}];
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
     expect(joinNames(inputNames)).toEqual("Bart, Lisa & Maggie");
